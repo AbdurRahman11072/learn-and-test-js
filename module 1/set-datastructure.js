8521c6f46d04bebcf21b42fed2a5a5d415c7b7bb
AbdurRahman11072 <rjrahman014@gmail.com>
2025-11-09 06:20:51 +0600

    first commit

---
+++ module 1/set-datastructure.js
@@ -0,0 +1,10 @@
+// ? learn dataSturcture of set
+//  * set doesn't content dublicate data it store unique data
+
+const set = new set();
+
+set.add("apple");
+set.add("banana");
+set.add("mango");
+set.add("apple");
+set.add("pineapple");
